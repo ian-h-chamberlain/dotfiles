5d148565b34decfebf7bddc66ba37cadeb95ddd7
Ian Chamberlain <ichamberlain@128technology.com>
2019-09-18 13:21:12 -0400

    Improve bazel completions more
    Various fixes and some speedups

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,11 @@ if not set -q DOCKER_NAME; and test -f /etc/profile.d/docker_name.sh
     set -gx DOCKER_NAME (sed -E 's/.*DOCKER_NAME=(.+)/\1/' /etc/profile.d/docker_name.sh)
 end
 
-if command -sq thefuck
-    thefuck --alias | source
+if status is-interactive; and status is-login
+    command -sq thefuck; and source (thefuck --alias | psub)
+
+    if command -qs pyenv
+        source (pyenv init - | psub)
+        source (pyenv virtualenv-init - | psub)
+    end
 end
