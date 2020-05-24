815254053fb069ee4be080d88d42235e15e964f7
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2020-05-24 12:47:56 -0400

    Update config to avoid missing commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,16 @@
-thefuck --alias f | source
+command -q thefuck
+    and thefuck --alias f | source
 
-# eval (ssh-agent -c) >/dev/null
-
-status --is-interactive; and pyenv init - | source
-status --is-interactive; and pyenv-virtualenv-init - | source
+status --is-interactive
+    and command -q pyenv
+    and pyenv init - | source
+status --is-interactive
+    and command -q pyenv-virtualenv
+    and pyenv-virtualenv-init - | source
 
 # Set here instead of in fish_variables to expand $HOME per-machine
-set -U fish_user_paths (npm bin) ~/.cargo/bin ~/.pyenv/shims /usr/local/Cellar/pyenv-virtualenv/*/shims
+command -q npm
+    and set -U fish_user_paths (npm bin)
+
+set -Ua fish_user_paths ~/.cargo/bin ~/.pyenv/shims /usr/local/Cellar/pyenv-virtualenv/*/shims
 
