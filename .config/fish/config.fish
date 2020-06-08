48ee5a64623760240978f69e31ed732bb294d5e9
Ian Chamberlain <ichamberlain@128technology.com>
2020-06-08 11:11:20 -0400

    Fix up merge with work VSCode settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,20 @@
+# Set fish_user_paths here instead of fish_variables to expand $HOME per-machine
+set -U fish_user_paths
+
+if command -qs npm
+    set -Ua fish_user_paths (npm bin)
+end
+
+set -Ua fish_user_paths ~/.cargo/bin ~/.pyenv/shims /usr/local/Cellar/pyenv-virtualenv/*/shims
+
 if not set -q DOCKER_NAME; and test -f /etc/profile.d/docker_name.sh
     set -gx DOCKER_NAME (sed -E 's/.*DOCKER_NAME=(.+)/\1/' /etc/profile.d/docker_name.sh)
 end
 
 if status is-interactive; and status is-login
-    command -sq thefuck; and source (thefuck --alias | psub)
+    if command -qs thefuck
+        source (thefuck --alias | psub)
+    end
 
     if command -qs pyenv
         source (pyenv init - | psub)
