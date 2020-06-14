da5e92a2bf0b1bb3cb5bb28844ff8feedc5154b9
Ian Chamberlain <ichamberlain@128technology.com>
2020-06-14 10:19:08 -0400

    Update yadm completions, + add iterm prefs
    Also update VSCode extensions.

--- .config/fish/completions/yadm.fish
+++ .config/fish/completions/yadm.fish
@@ -1,111 +1,78 @@
-# Normal git completion
-complete -c yadm --wraps git
+# Derived from https://github.com/TheLocehiliosan/yadm/pull/224
 
-if ! functions -q __fish_git_using_command
-    source $__fish_data_dir/completions/git.fish
+function __fish_yadm_universal_optspecs
+    string join \n 'a-yadm-dir=' 'b-yadm-repo=' 'c-yadm-config=' \
+                    'd-yadm-encrypt=' 'e-yadm-archive=' 'f-yadm-bootstrap='
 end
 
-function gitconfig_completions
-    # gitconfig options, copied from /usr/local/share/fish/completions/git.fish
-    # Complete both options and possible parameters to `git config`
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l global -d 'Get/set global configuration'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l system -d 'Get/set system configuration'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l local -d 'Get/set local repo configuration'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -s f -l file -d 'Read config from file'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l blob -d 'Read config from blob' -ra '(__fish_complete_suffix '')'
-
-    # If no argument is specified, it's as if --get was used
-    complete -c yadm -n '__fish_git_using_command gitconfig; and __fish_is_token_n 3' -fa '(__fish_git_config_keys)'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig; and __fish_is_first_arg' -l get -d 'Get config with name' -ra '(__fish_git_config_keys)'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l get -d 'Get config with name' -ra '(__fish_git_config_keys)'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l get-all -d 'Get all values matching key' -a '(__fish_git_config_keys)'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l get-urlmatch -d 'Get value specific for the section url' -r
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l replace-all -d 'Replace all matching variables' -ra '(__fish_git_config_keys)'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l add -d 'Add a new variable' -r
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l unset -d 'Remove a variable' -a '(__fish_git_config_keys)'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l unset-all -d 'Remove matching variables' -a '(__fish_git_config_keys)'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l rename-section -d 'Rename section' -r
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -s l -l list -d 'List all variables'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -s e -l edit -d 'Open configuration in an editor'
-
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -s t -l type -d 'Value is of given type'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l bool -d 'Value is \'true\' or \'false\''
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l int -d 'Value is a decimal number'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l bool-or-int -d 'Value is --bool or --int'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l path -d 'Value is a path'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l expiry-date -d 'Value is an expiry date'
-
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -s z -l null -d 'Terminate values with NUL byte'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l name-only -d 'Show variable names only'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l includes -d 'Respect include directives'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig' -l show-origin -d 'Show origin of configuration'
-    complete -f -c yadm -n '__fish_git_using_command gitconfig; and __fish_seen_argument get' -l default -d 'Use default value when missing entry'
+function __fish_yadm_needs_command
+    # Figure out if the current invocation already has a command.
+    set -l cmd (commandline -opc)
+    set -e cmd[1]
+    argparse -s (__fish_yadm_universal_optspecs) -- $cmd 2>/dev/null
+    or return 0
+    if set -q argv[1]
+        echo $argv[1]
+        return 1
+    end
+    return 0
 end
 
-# In general, piggyback off git helper functions for subcommands
+function __fish_yadm_using_command
+    set -l cmd (__fish_yadm_needs_command)
+    test -z "$cmd"
+    and return 1
+    contains -- $cmd $argv
+    and return 0
+end
 
-# TODO: `yadm add` does not complete paths the same way as `git add`.
-# May be easier to just overwrite the default completion completely
+# yadm's specific autocomplete
+complete -x -c yadm -n '__fish_yadm_needs_command'       -a 'clone'      -d 'Clone an existing repository'
+complete -F -c yadm -n '__fish_yadm_using_command clone' -s w            -d 'work-tree to use (default: $HOME)'
+complete -f -c yadm -n '__fish_yadm_using_command clone' -s b            -d 'branch to clone'
+complete -x -c yadm -n '__fish_yadm_using_command clone' -s f            -d 'force to overwrite'
+complete -x -c yadm -n '__fish_yadm_using_command clone' -l bootstrap    -d 'force bootstrap to run'
+complete -x -c yadm -n '__fish_yadm_using_command clone' -l no-bootstrap -d 'prevent bootstrap from beingrun'
 
-# Based on output of `yadm introspect switches`:
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -l yadm-archive -d 'yadm: Override the location of the yadm encrypted files archive'
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -l yadm-bootstrap -d 'yadm: Override the location of the yadm bootstrap program'
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -l yadm-config -d 'yadm: Override the location of the yadm configuration file'
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -s Y -l yadm-dir -d 'yadm: Override the yadm directory.'
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -l yadm-encrypt -d 'yadm: Override the location of the yadm encryption configuration'
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -l yadm-repo -d 'yadm: Override the location of the yadm repository'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'alt'       -d 'Create links for alternates'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'bootstrap' -d 'Execute $HOME/.config/yadm/bootstrap'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'perms'     -d 'Fix perms for private files'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'enter'     -d 'Run sub-shell with GIT variables set'
+complete    -c yadm -n '__fish_yadm_needs_command' -a 'git-crypt' -d 'Run git-crypt commands for the yadm repo'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'help'      -d 'Print a summary of yadm commands'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'upgrade'   -d 'Upgrade to version 2 of yadm directory structure'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'version'   -d 'Print the version of yadm'
 
-# Descriptions taken from yadm man pages
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a alt -d 'yadm: Create symlinks for managed files matching alt naming rules'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'init' -d 'Initialize an empty repository'
+complete -x -c yadm -n '__fish_yadm_using_command init' -s f -d 'force to overwrite'
+complete -F -c yadm -n '__fish_yadm_using_command init' -s w -d 'set work-tree (default: $HOME)'
 
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a bootstrap -d 'yadm: Execute $HOME/.config/yadm/bootstrap if it exists.'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'list' -d 'List tracked files at current directory'
+complete -x -c yadm -n '__fish_yadm_using_command list' -s a -d 'list all managed files instead'
 
-complete -x -c yadm -n '__fish_git_using_command clone' \
-    --long-option bootstrap -d 'yadm: Automatically run bootstrap on successful clone' \
-    --long-option no-bootstrap -d 'yadm: Prevent bootstrap from running on successful clone'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'encrypt' -d 'Encrypt files'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'decrypt' -d 'Decrypt files'
+complete -x -c yadm -n '__fish_yadm_using_command decrypt' -s l -d 'list the files stored without extracting'
 
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a decrypt -d 'yadm: decrypt files stored in ~/.config/yadm/files.gpg'
-complete -f -c yadm -n '__fish_git_using_command decrypt' \
-    --short-option l -d 'yadm: List files without decrypting them'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'introspect' -d 'Report internal yadm data'
+complete -x -c yadm -n '__fish_yadm_using_command introspect' -a (printf -- '%s\n' 'commands configs repo switches') -d 'category'
 
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a encrypt -d 'yadm: Encrypt all files matching the patterns in ~/.config/yadm/encrypt'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'gitconfig' -d 'Pass options to the git config command'
+complete -x -c yadm -n '__fish_yadm_needs_command' -a 'config'    -d 'Configure a setting'
+for name in (yadm introspect configs)
+    complete -x -c yadm -n '__fish_yadm_using_command config' -a "$name" -d 'yadm config'
+end
 
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a enter -d 'yadm: Run a sub-shell with all Git variables set'
+# yadm universal options
+complete --force-files -c yadm -s Y -l yadm-dir       -d 'Override location of yadm directory'
+complete --force-files -c yadm      -l yadm-repo      -d 'Override location of yadm repository'
+complete --force-files -c yadm      -l yadm-config    -d 'Override location of yadm configuration file'
+complete --force-files -c yadm      -l yadm-encrypt   -d 'Override location of yadm encryption configuration'
+complete --force-files -c yadm      -l yadm-archive   -d 'Override location of yadm encrypted files archive'
+complete --force-files -c yadm      -l yadm-bootstrap -d 'Override location of yadm bootstrap program'
 
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a gitconfig -d 'yadm: Pass options to the "git config" command'
+set -l GIT_DIR (yadm introspect repo)
+set -l GIT_WORK_TREE (yadm enter 'printf -- "%s" $GIT_WORK_TREE')
 
-gitconfig_completions
-
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a help -d 'yadm: Print a summary of yadm commands'
-
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a list -d 'yadm: print a List of files managed by yadm'
-complete -f -c yadm -n '__fish_git_using_command list' \
-    --short-option a -d 'yadm: List all managed files, instead of current dir or below'
-
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a introspect -d 'yadm: Report internal yadm data'
-complete -f -c yadm -n '__fish_git_using_command introspect' \
-    -a 'commands configs repo switches'
-
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a perms -d 'yadm: Update permissions as described in the PERMISSIONS section'
-
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a upgrade -d 'yadm: Upgrade from version 1 configuration to version 2 configuration'
-
-complete -f -c yadm -n '__fish_git_needs_command' \
-    -a version -d 'yadm: Print the version of yadm'
+# setup variables for git completion using `yadm enter`
+complete -c yadm -w "git --git-dir='$GIT_DIR' --work-tree='$GIT_WORK_TREE'"
