14fe7db39e97531c54560413f7a5ca3245f4eb2a
Ian Chamberlain <ichamberlain@128technology.com>
2020-07-21 11:20:47 -0400

    Simplify refresh_branches

--- .config/fish/functions/refresh_branches.fish
+++ .config/fish/functions/refresh_branches.fish
@@ -1,7 +1,7 @@
 function refresh_branches
     date
     echo
-	echo "=========================================================================="
+    echo "=========================================================================="
 
     echo "Pruning old docker images, containers, etc."
     if type -q docker
@@ -9,50 +9,26 @@ function refresh_branches
     end
     echo
 
-	set -l branches \
+    set -l branches \
         "develop" \
         "release/4.5" \
         "release/4.4" \
         "release/4.3" \
         "release/4.2" \
-        "feature/rbac"
+        "release/5.0"
 
-	if test (uname) != "Darwin"
+    if test (uname) != "Darwin"
         set dlib (/usr/local/bin/gbase)"/tools/dlib.sh"
     end
-
-    git stash push -m "temporary stash to update branches" | grep --quiet "No local changes to save"
-    set stashed_git_changes $status
-
-    function cleanup --inherit-variable stashed_git_changes
-        if test $stashed_git_changes = 1
-            git stash pop
-        end
-
-        echo "=========================================================================="
-        echo
-    end
-
-    trap cleanup EXIT
 
     for branch in $branches
         echo "Refreshing upstream branch '$branch'"
 
-        git checkout $branch
-        if ! git pull --prune --ff-only origin $branch
+        if ! git fetch origin $branch:$branch
             echo "Failed to pull '$branch'"
-            git checkout -
-            echo
             continue
         end
 
-        if set -q dlib
-            echo "Pulling docker images with dlib '$dlib'"
-            bash -c "source $dlib && dlib_pull_release"
-        end
-
-        # Restore the previous branch
-        git checkout -
         echo
     end
 end
