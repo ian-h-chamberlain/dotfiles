4fd55918cca4df42b284bcecd017e4582c068140
Ian Chamberlain <ichamberlain@128technology.com>
2020-12-14 11:08:31 -0500

    Minor updates for gdb + dlib helper

--- .config/gdb/init.py
+++ .config/gdb/init.py
@@ -6,8 +6,15 @@ import re
 
 # Replace working dir /i95code with ~/Documents/workspace
 # This make things like cmd+click work properly
-pwd_str = gdb.execute("pwd", from_tty=False, to_string=True).strip()
+# pwd_str = gdb.execute("pwd", from_tty=False, to_string=True).strip()
+# 
+# new_pwd = re.sub(r"^.+/i95code/(.+)\.$", r"~/Documents/workspace/\1", pwd_str)
+# print("cwd: '{}'".format(new_pwd))
+# gdb.execute("cd {}".format(new_pwd))
 
-new_pwd = re.sub(r"^.+/i95code/(.+)\.$", r"~/Documents/workspace/\1", pwd_str)
-print("cwd: '{}'".format(new_pwd))
-gdb.execute("cd {}".format(new_pwd))
+# TODO: is it possible to modify the file here instead of just reminding myself?
+print(
+    "If function names are showing as blaack, it may be due to "
+    "<https://bugzilla.redhat.com/show_bug.cgi?id=1688372>. Try "
+    "manually editing /usr/share/source-highlight/esc.style"
+)
