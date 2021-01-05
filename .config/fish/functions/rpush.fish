2156170899f6fae9e04899af77b19e25dee6dad0
Ian Chamberlain <ichamberlain@juniper.net>
2021-01-05 13:58:22 -0500

    Update vscode extensions + rpush

--- .config/fish/functions/rpush.fish
+++ .config/fish/functions/rpush.fish
@@ -1,4 +1,12 @@
 function rpush
 	source ~/.config/fish/functions/remote_cmd.fish
-	remote_cmd rpush $argv && greset
+	remote_cmd rpush $argv 
+
+    set rpush_status $status
+    if test $rpush_status -ne 0
+        echo (set_color yellow)"WARNING:"(set_color normal) \
+            "rpush exited with code $rpush_status" >&2
+    end
+
+    greset
 end
