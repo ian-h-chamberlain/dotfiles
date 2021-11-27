bae5428f3b49db78242f77fdadff86ef87e4c816
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2021-11-27 10:51:18 -0500

    Set cask appdir on personal, cargo completions

--- .config/fish/completions/cargo.fish
+++ .config/fish/completions/cargo.fish
@@ -1,10 +1,10 @@
-# Override function in /usr/local/share/fish/completions/cargo.fish
+source /usr/local/share/fish/completions/cargo.fish
+
+# https://github.com/fish-shell/fish-shell/issues/8429
 function __list_cargo_examples
     if not test -d ./examples
         return
     end
 
-    find ./examples -mindepth 1 \( \
-        -type f -name "*.rs" -or -type d \
-        \) -exec basename '{}' \; | string replace '.rs' ''
+    cargo run --example 2>&1 | string replace -rf '^\s+' ''
 end
