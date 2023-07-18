bc9bcdc18c71dd38af6def55acbb26fc19481ef1
Ian Chamberlain <ichamberlain@juniper.net>
2023-07-18 12:55:07 -0400

    Add misc. fish functions and test logs to lnav

---
+++ .config/fish/functions/cpy.fish
@@ -0,0 +1,4 @@
+function cpy --wraps=fish_clipboard_copy --description 'alias cpy fish_clipboard_copy'
+    fish_clipboard_copy $argv
+
+end
---
+++ .config/fish/functions/funcdel.fish
@@ -0,0 +1,4 @@
+function funcdel --wraps='functions --erase' --description 'alias funcdel functions --erase'
+    functions --erase $argv
+
+end
