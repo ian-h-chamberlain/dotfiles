ecf76a1fb627e0fd7bb0b3dbc1f265a990148a8f
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2023-12-22 22:41:52 -0500

    Swap to using separate brewfiles
    Prune a number of brew entries and split up more by purpose (vscode,
    operating system, etc.)

--- .config/fish/conf.d/25-brew.fish
+++ .config/fish/conf.d/25-brew.fish
@@ -1,3 +1,7 @@
+if command -q yadm; and test (yadm config local.class) = personal
+    set -gx HOMEBREW_CASK_OPTS "--appdir=$HOME/Applications"
+end
+
 # Set up brew PATH when it's installed in unusual places
 if status is-interactive
     if test -x /home/linuxbrew/.linuxbrew/bin/brew
@@ -7,3 +11,10 @@ if status is-interactive
         eval (/opt/homebrew/bin/brew shellenv)
     end
 end
+
+# This lets us use `brew bundle add --global` for cross-system packages
+# and `brew bundle add` for system-specific packages.
+set -gx HOMEBREW_BUNDLE_FILE_GLOBAL ~/.config/brew/Brewfile
+set -gx HOMEBREW_BUNDLE_FILE ~/.config/brew/class.Brewfile
+# For https://github.com/superatomic/homebrew-bundle-extensions/pull/14
+set -gx HOMEBREW_BUNDLE_DUMP_DESCRIBE 1
