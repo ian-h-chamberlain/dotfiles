ada27021ab785e0e850eea4fe91e7ea10d2ce02c
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2024-08-18 08:49:57 -0400

    Fix numbertoggle in PR comments

--- .config/nvim/plugin/numbertoggle.lua
+++ .config/nvim/plugin/numbertoggle.lua
@@ -2,32 +2,30 @@
 
 local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
 
-vim.api.nvim_create_autocmd(
-    { "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter", "BufWinEnter" },
-    {
-        pattern = "*",
-        group = augroup,
-        callback = function(evt)
-            if vim.api.nvim_get_mode().mode ~= "i" then
-                vim.opt_local.relativenumber = true
-                -- Seems that in vscode-neovim this is not automatically
-                -- executed for some reason... Possibly a neovim bug?
-                vim.api.nvim_exec_autocmds("OptionSet", { pattern = "relativenumber" })
-                vim.cmd.redraw()
-            end
-        end,
-    }
-)
-
-vim.api.nvim_create_autocmd(
-    { "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave", "BufWinLeave", },
-    {
-        pattern = "*",
-        group = augroup,
-        callback = function(evt)
-            vim.opt_local.relativenumber = false
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter", "BufWinEnter" }, {
+    pattern = "*",
+    group = augroup,
+    callback = function(evt)
+        -- Skip if vim.o.number is unset, for e.g. PR comment editor
+        if vim.api.nvim_get_mode().mode ~= "i" and vim.o.number then
+            vim.o.relativenumber = true
+            -- Seems that in vscode-neovim this is not automatically
+            -- executed for some reason... Possibly a neovim bug?
             vim.api.nvim_exec_autocmds("OptionSet", { pattern = "relativenumber" })
             vim.cmd.redraw()
-        end,
-    }
-)
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave", "BufWinLeave" }, {
+    pattern = "*",
+    group = augroup,
+    callback = function(evt)
+        -- Skip if vim.o.number is unset, for e.g. PR comment editor
+        if vim.o.number then
+            vim.o.relativenumber = false
+            vim.api.nvim_exec_autocmds("OptionSet", { pattern = "relativenumber" })
+            vim.cmd.redraw()
+        end
+    end,
+})
