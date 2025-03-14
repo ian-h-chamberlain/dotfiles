fa2342b1ff5038ad4c0baa30dbf2736c7c2b5f03
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2025-03-14 17:19:54 -0400

    Miscellaneous git/fish configurations
    - Check in config for gh and gitkeep dirs for gdb + emacs
    - Misc. gitignore, git aliases, and SSH config updates
    - Various fish completion fixes, functions, etc.

--- .config/fish/functions/upfind.fish
+++ .config/fish/functions/upfind.fish
@@ -1,7 +1,16 @@
 function upfind --description 'Finds a file with the given name, upwards'
+    set -l usage "Usage:
+    upfind [-a|--all] [-d DIR|--from=DIR] FILE"
+
+    argparse --min-args=1 a/all d/from= -- $argv
+    or begin
+        echo $usage
+        return
+    end
+
     set dir $PWD
-    if set -q argv[2]
-        set dir $argv[2]
+    if set -q _flag_from
+        set dir $_flag_from
     end
 
     test (builtin realpath --no-symlinks $dir) != /
@@ -9,7 +18,10 @@ function upfind --description 'Finds a file with the given name, upwards'
 
     if test -e $dir/$argv[1]
         builtin realpath --no-symlinks $dir/$argv[1]
-    else
-        upfind $argv[1] $dir/..
+        if not set -ql _flag_all
+            return
+        end
     end
+
+    upfind $argv[1] --from=$dir/..
 end
